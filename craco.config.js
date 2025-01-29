cca7b5bc1c7001aee0343678d339f0dde51faa1d
Goppie <goppie@Goppies-MacBook-Pro.local>
2025-01-29 15:47:21 -0700

    Update build configuration for TypeScript compatibility

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   reactScriptsVersion: "react-scripts",
   style: {
@@ -8,5 +10,18 @@ module.exports = {
         };
       },
     },
+  },
+  webpack: {
+    alias: {
+      'typescript': path.resolve(__dirname, 'node_modules/typescript'),
+    },
+    configure: (webpackConfig) => {
+      // Force webpack to resolve typescript to our version
+      webpackConfig.resolve = webpackConfig.resolve || {};
+      webpackConfig.resolve.alias = webpackConfig.resolve.alias || {};
+      webpackConfig.resolve.alias.typescript = path.resolve(__dirname, 'node_modules/typescript');
+      
+      return webpackConfig;
+    }
   }
 };
